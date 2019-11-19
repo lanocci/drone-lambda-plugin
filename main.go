c5938ff16d004d2bc69f0559950d5522d10541c6
lanocci <kurn17.kkum@gmail.com>
2019-11-19 11:56:13 +0900

    add region parameter

--- main.go
+++ main.go
@@ -7,19 +7,29 @@ import (
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/aws/awserr"
     "github.com/aws/aws-sdk-go/aws"
+    "github.com/kelseyhightower/envconfig"
 )
+
+type Config struct {
+	FunctionName string `envconfig:"PLUGIN_FUNCTION_NAME" requeired:"true"`
+	S3Bucket string `envconfig:"PLUGIN_S3_BUCKET" required:"true"`
+	S3Key string `envconfig:"PLUGIN_FILE_NAME" required:"true"`
+	Region string `envconfig:"PLUGIN_LAMBDA_REGION" default:"us-east-1"`
+}
 
 
 func main() {
+	var conf Config
+	envconfig.Process("", &conf)
     svc := lambda.New(session.New(&aws.Config{
-        Region: aws.String("us-east-1"),
+        Region: aws.String(conf.Region),
     }))
 
     input := &lambda.UpdateFunctionCodeInput{
-        FunctionName:    aws.String(os.Getenv("PLUGIN_FUNCTION_NAME")),
+        FunctionName:    aws.String(conf.FunctionName),
         Publish:         aws.Bool(true),
-        S3Bucket:        aws.String(os.Getenv("PLUGIN_S3_BUCKET")),
-        S3Key:           aws.String(os.Getenv("PLUGIN_FILE_NAME")),
+        S3Bucket:        aws.String(conf.S3Bucket),
+        S3Key:           aws.String(conf.S3Key),
     }
 
     result, err := svc.UpdateFunctionCode(input)
